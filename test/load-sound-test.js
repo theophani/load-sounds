8c3e9d0f80e40ecf6f1bc7994802217a4c1577e1
Tiffany Conroy <tiffany.conroy@gmail.com>
2012-07-04 22:28:36 +0200

    adding multiple sounds to the tests

--- test/load-sound-test.js
+++ test/load-sound-test.js
@@ -10,6 +10,10 @@ var err = function (message, request) {
 
 var request = loadSound('woody_5.ogg',  context, callback, err);
 
+var second_request = setTimeout(function () {
+		loadSound('woody_3.ogg',  context, callback, err);
+	}, 500);
+
 var request_failed = loadSound('wrong_file_name.ogg',  context, callback, err);
 
 var unhandled_error = loadSound('another_wrong_file.ogg',  context, callback);
--- test/load-sounds-test.js
+++ test/load-sounds-test.js
@@ -1,6 +1,7 @@
 var paths = {
 	'woody1': 'woody_1.ogg',
-	'woody2': 'wrong_file_name.ogg'
+	'woody2': 'wrong_file_name.ogg',
+	'woody5': 'woody_5.ogg',
 };
 
 var ready = function () {
@@ -9,6 +10,11 @@ var ready = function () {
 	}
 	if (sounds.woody2) {
 		playSound(sounds.woody2);
+	}
+	if (sounds.woody5) {
+		setTimeout(function () {
+			playSound(sounds.woody5);
+		}, 500);
 	}
 };
 

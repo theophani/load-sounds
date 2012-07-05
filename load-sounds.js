f1c811952ef9cfbd4e549bb3ef3115839a86c3bf
Tiffany Conroy <tiffany.conroy@gmail.com>
2012-07-05 02:45:11 +0200

    give error message when audioContext fails

--- load-sounds.js
+++ load-sounds.js
@@ -21,12 +21,12 @@ var loadSounds = function(paths, callback, progress) {
 		var done = (loaded.length + errors.length) === paths.length;
 
 		// if callback exists, execute it after all sounds have loaded
-		if (done && callback) {
+		if (done && typeof callback === 'function') {
 			callback(sounds);
 		}
 
 		// progress is an optional intermediate callback
-		if (progress) {
+		if (typeof progress === 'function') {
 			progress(sounds);
 		}
 	};

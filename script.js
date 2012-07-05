8b00b8f6c4b7c24d60480cf4a2d4db82b8c12abf
Tiffany Conroy <tiffany.conroy@gmail.com>
2012-07-05 02:07:16 +0200

    debug attempt

--- script.js
+++ script.js
@@ -30,9 +30,14 @@ var addKeys = function (sounds) {
 };
 
 var ready = function (sounds) {
+	var p = document.createElement('p');
+	p.innerHTML = requests.errors
+	document.body.appendChild(p);
+
+
 	sounds.forEach(function (sound, i) {
 		setTimeout(function () {
-			//playSound(sound);
+			playSound(sound);
 			makeDiv(i, sound);
 		}, 100 * i);
 	});
